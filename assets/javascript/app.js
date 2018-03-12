d7bdb73f8620f749d01e6e1ad803ba2cc0ebb74f
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-03-12 14:48:05 -0700

    fixed https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
   // Function to get gifs from Giphy
     function displayGifs(){
         var action = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
         $.ajax({
             url: queryURL,
             method: 'GET'
